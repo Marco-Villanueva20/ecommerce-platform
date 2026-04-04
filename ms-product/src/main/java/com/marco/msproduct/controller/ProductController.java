5a1794cea4db44506eb21c155fb0f91e56181631
Marco Villanueva Soto <marco_x12305@hotmail.com>
2026-04-04 05:56:26 -0500

    Implement Product CRUD stubs in Product Service
    - **Product Service**: Implement REST endpoint stubs for `getById`, `create`, `getAll`, `update`, and `delete` operations in `ProductController`.
    - **Product Service**: Inject `ProductService` and `ProductMapper` dependencies into `ProductController` to support future business logic and data transformation.

--- ms-product/src/main/java/com/marco/msproduct/controller/ProductController.java
+++ ms-product/src/main/java/com/marco/msproduct/controller/ProductController.java
@@ -1,11 +1,48 @@
 package com.marco.msproduct.controller;
 
+import com.marco.msproduct.dto.ProductRequest;
+import com.marco.msproduct.entity.Product;
+import com.marco.msproduct.mapper.ProductMapper;
+import com.marco.msproduct.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/products")
 public class ProductController {
+
+    private final ProductService productService;
+    private final ProductMapper productMapper;
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getById(@PathVariable Long id){
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> create(@Valid @RequestBody ProductRequest request, UriComponentsBuilder ucb){
+        URI location = ucb.cloneBuilder().path("api/v1/products/{id}").buildAndExpand(12).toUri();
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll(){
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody ProductRequest request){
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        return null;
+    }
 }
